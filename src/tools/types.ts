2fd993f4614970d22f7f757d4bde38a531d600df
Akash Pramanik <akashpramanik422@gmail.com>
2026-08-30 18:17:03 +0530

    Adds password field type support
    Enables tool fields to be configured as password inputs, allowing secure handling of sensitive values.

--- src/tools/types.ts
+++ src/tools/types.ts
@@ -1,4 +1,4 @@
-﻿export type ToolFieldType = 'number' | 'select' | 'text' | 'color' | 'font'
+﻿export type ToolFieldType = 'number' | 'select' | 'text' | 'color' | 'font' | 'password'
 
 export interface ToolField {
   name: string
